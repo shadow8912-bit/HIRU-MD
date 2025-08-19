5dd0b98a4df946e45130a6b84df4cf08f0d40d2e
shadow8912-bit <oshithadasuni8912@gmail.com>
2025-08-19 20:42:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || " sjsnssjshhshrhrbbb", // ඔයාගෙ session id එක
+SESSION_ID: process.env.SESSION_ID || "&WQcSFDaA#Wy8TzQevHwZqHbK_K0f1gdkBQ2y-oyF5wjSqjKc-Syo", // ඔයාගෙ session id එක
 MONGODB: process.env.MONGODB || "mongodb://mongo:sjjkkd",  //ඔයාගෙ mongoDb url එක
 };
  
